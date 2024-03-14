cc54624cf64afaa08bee4fe0e7c2607041104c07
Samuel Cornet <corsam28@gmail.com>
2024-03-14 14:42:55 -0500

    Create the use model

---
+++ src/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      validate: validator.isEmail,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
