d7f36348dfdb6a45f933fbf86fd3408a038029f3
Samuel Cornet <corsam28@gmail.com>
2024-03-14 13:44:46 -0500

    Restructor the backend

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+// import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+import colors from "colors";
+
+
+import testRoutes from "./src/routes/testRoutes.js";
+
+//Dot ENV config
+dotenv.config();
+
+// mongodb connection
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/api/v1/test", testRoutes)
+// port
+const PORT = process.env.PORT || 5000;
+const server = http.createServer(app);
+server.listen(PORT, () => {
+    console.log(`Server is running in ${process.env.DEV_MODE} and listening on port ${PORT}`.bgCyan.white);
+});
