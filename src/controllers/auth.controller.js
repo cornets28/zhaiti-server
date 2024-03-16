edab7edf3445ce5723d9a370bed8c936b52a7ff9
Samuel Cornet <corsam28@gmail.com>
2024-03-16 17:29:49 -0500

    Add JWT

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,36 +1,57 @@
 import userModel from "../models/user.model.js";
 
 const registerController = async (req, res, next) => {
-    const { firstName, lastName, email, password } = req.body;
-    const existingUser = await userModel.findOne({ email });
+  const { firstName, lastName, email, password } = req.body;
+  const existingUser = await userModel.findOne({ email });
 
-    if (!firstName) {
-      next("First name is required");
-    }
-    if (!lastName) {
-      next("last name is required");
-    }
-    if (!email) {
-      next("Email is required");
-    }
-    if (!password) {
-      next("Password is required and should be greater than 8 characters");
-    }
+  if (!firstName) {
+    next("First name is required");
+  }
+  if (!lastName) {
+    next("last name is required");
+  }
+  if (!email) {
+    next("Email is required");
+  }
+  if (!password) {
+    next("Password is required and should be greater than 8 characters");
+  }
 
-    if (existingUser) {
-      next("Email already exists, Please login.");
-    }
-    const user = await userModel.create({
-      firstName,
-      lastName,
-      email,
-      password,
-    });
+  if (existingUser) {
+    next("Email already exists, Please login.");
+  }
+  const user = await userModel.create({
+    firstName,
+    lastName,
+    email,
+    password,
+    // sex,
+    // dob,
+    // address,
+    // image_url,
+    // nationality,
+    // role,
+    // occupation,
+  });
 
-    res
-      .status(201)
-      .send({ success: true, message: "User created successfully.", user });
-  } 
-
+  const token = await user.createJWT();
+  res.status(201).send({
+    success: true,
+    message: "User created successfully.",
+    user: {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+    //   sex: user.sex,
+    //   dob: user.dob,
+    //   address: user.address,
+    //   image_url: user.image_url,
+    //   nationality: user.nationality,
+    //   role: user.role,
+    //   occupation: user.occupation,
+    },
+    token,
+  });
+};
 
 export default registerController;
