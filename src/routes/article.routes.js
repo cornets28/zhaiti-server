ba0dda324809cce62862fc8645f99c566f7bacfe
Samuel Cornet <corsam28@gmail.com>
2024-03-18 15:17:56 -0500

    Allow user to update an article

--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -3,19 +3,23 @@ import {
   createArticleController,
   getAllArticlesController,
   getUserArticlesController,
+  updateArticlesController
 } from "../controllers/article.controller.js";
 import userAuth from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 // routes
-// CREATE ARTICLE
+// CREATE ARTICLE || POST
 router.post("/create-article", userAuth, createArticleController);
 
-// GET ARTICLES
+// GET ARTICLES || GET
 router.get("/get-articles", userAuth, getAllArticlesController);
 
-// GET USER ARTICLES
+// GET USER ARTICLES || GET
 router.get("/user/get-articles", userAuth, getUserArticlesController);
+
+//UPDATE ARTICLE ||  PATCH || PUT
+router.patch("/update-article/:id", userAuth, updateArticlesController);
 
 export default router;
