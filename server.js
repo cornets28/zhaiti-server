371af7bca11e18764c0c26c9c1c29f153d4b18a2
Samuel Cornet <corsam28@gmail.com>
2024-03-23 15:10:38 -0500

    Create login and sign up endpoints in the swagger

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ import dotenv from "dotenv";
 import colors from "colors";
 import morgan from "morgan";
 import "express-async-errors";
+// API documentation
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 // security
 import helmet from "helmet";
 import xss from "xss-clean";
@@ -17,7 +20,7 @@ import connectDB from "./src/config/db.js";
 import authRoutes from "./src/routes/auth.routes.js";
 import errorMiddleware from "./src/middlewares/error.middleware.js";
 import userRoutes from "./src/routes/user.routes.js";
-import articleRoutes from "./src/routes/article.routes.js"
+import articleRoutes from "./src/routes/article.routes.js";
 
 //Dot ENV config
 dotenv.config();
@@ -25,8 +28,32 @@ dotenv.config();
 // mongodb connection
 connectDB();
 
+// Swagger API configuration
+// API configuration options
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "ZHaiti news application",
+      version: "1.0.0",
+      description:
+        "ZHaiti news application is a web a web application for interacting with the ZHaiti front-end",
+    },
+    servers: [
+      {
+        // url: `http://localhost:${process.env.PORT}`,
+        url: "http://localhost:5000"
+      },
+    ],
+  },
+  apis: ["./src/routes/*.js"],
+};
+
+const spec = swaggerJsdoc(options);
+
 // rest object
 const app = express();
+
 
 // middlewares
 app.use(helmet());
@@ -41,6 +68,9 @@ app.use(morgan("dev"));
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/article", articleRoutes);
+
+// home route root
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(spec));
 
 // validation middleware
 app.use(errorMiddleware);
