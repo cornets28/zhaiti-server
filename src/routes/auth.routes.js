59c6ebb6495ee9cddd11d22349b2839d4a5fcbb0
Samuel Cornet <corsam28@gmail.com>
2024-03-22 20:27:08 -0500

    Ass security

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,24 @@
-import express from 'express';
-import {loginController, registerController} from '../controllers/auth.controller.js';
-import userAuth from '../middlewares/auth.middleware.js';
+import express from "express";
+import {
+  loginController,
+  registerController,
+} from "../controllers/auth.controller.js";
+import rateLimit from "express-rate-limit";
 
+//ip limiter
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
+// router objects
 const router = express.Router();
 
-// routes
-router.post('/register', registerController)
-router.post('/login', loginController)
+// REGISTER || POPST
+router.post("/register", limiter, registerController);
+// LOGIN || POST
+router.post("/login", limiter, loginController);
 
 export default router;
